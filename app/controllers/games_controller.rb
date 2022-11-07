f11fa0d913e3516a67ceaba8790e25a554c22d2e
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2022-11-07 23:15:21 +0800

    wtf is this

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,32 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+
+def new
+
+  @letters = [*'A'..'Z'].shuffle[0..8]
+
+end
+
+def score
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  # private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
+
+end
